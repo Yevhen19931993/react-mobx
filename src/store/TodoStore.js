9dbcef89bcf56c0f638c45e2065275430b091a9d
Yevhen_Tiaka <tiaka.lanp@gmail.com>
2023-09-01 13:11:32 +0300

    Commit

--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -5,50 +5,14 @@ class TodoStore {
 
   constructor() {
     makeAutoObservable(this);
-    this.fetchTodos();
   }
 
-  fetchTodos = async () => {
-    try {
-      const response = await fetch('http://localhost:3001/todos');
-      const data = await response.json();
-      this.todos = data;
-    } catch (error) {
-      console.error('Ошибка при загрузке задач:', error);
-    }
+  addTodo = (text) => {
+    this.todos.push({ text, id: Date.now() });
   };
 
-  addTodo = async (text) => {
-    try {
-      const response = await fetch('http://localhost:3001/todos', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text }),
-      });
-
-      if (response.ok) {
-        const newTodo = await response.json();
-        this.todos.push(newTodo);
-      }
-    } catch (error) {
-      console.error('Ошибка при добавлении задачи:', error);
-    }
-  };
-
-  removeTodo = async (id) => {
-    try {
-      const response = await fetch(`http://localhost:3001/todos/${id}`, {
-        method: 'DELETE',
-      });
-
-      if (response.ok) {
-        this.todos = this.todos.filter((todo) => todo.id !== id);
-      }
-    } catch (error) {
-      console.error('Ошибка при удалении задачи:', error);
-    }
+  removeTodo = (id) => {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
   };
 }
 
